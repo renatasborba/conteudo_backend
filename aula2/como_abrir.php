55c2dd99d74a78e00f14246c89346d79adbcc79e
Renata Silva Borba <12253718645@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-02-14 08:44:04 -0300

    first commit

---
+++ aula2/como_abrir.php
@@ -0,0 +1,12 @@
+<?php
+    /*dar dois cliques no arquivo chamado
+    como_abrir.php, após substituir na
+    barra de endereços do navegador
+    o file://c:/xampp/htdocs por
+    http://localhost
+
+    exemplo:
+    http://localhost/3infob/renata/aula2/como_abrir.php */
+    
+    echo "imprimindo algo na tela";
+?>
