d78e3a74003bc8ea9ce26ce5fdfb207e8fd7a2f8
Renata <renata.borba@alunos.ifsuldeminas.edu.br>
2023-03-14 08:42:14 -0300

    aula8

--- aula8/vetor2.php
+++ aula8/vetor2.php
@@ -27,6 +27,14 @@ var_dump($alunos); //para ver os valores dentro da varável
 echo $alunos["nota1"]; //imprimir o valor da nota
 
 $alunos["notatotal"] = ($alunos["nota1"] + $alunos["nota2"]); //somar os índices
-var_dump($alunos);
+var_dump($alunos) ;
 
+echo "<p>&nbsp;</p><pre>";
+//imprimir todos os valores
+//utilizando o foreach
+foreach($alunos as $indice => $valor){ //imprime o que tem dentro do vetor
+        printf("%24s\n", "+------------+");
+        printf("%9s | %10s |  \n", $indice, $valor);
+        }
 ?>
+</pre>
