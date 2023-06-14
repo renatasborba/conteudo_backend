69e6fcc1e50462dbc90633baccc30d7803e01b49
renata <renata.borba@alunos.ifsuldeminas.edu.br>
2023-06-14 09:17:57 -0300

    att

--- aula14/application/controllers/Produto.php
+++ aula14/application/controllers/Produto.php
@@ -3,10 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Produto extends CI_Controller {
 
-
 	public function index()
 	{
-        echo "ola mudo";
-		//$this->load->view('welcome_message');
+        $crud = new Grocery_CRUD();
+        $crud->set_table("produto");
+        $crud->set_field_upload("foto");
+        $form = $crud->render();
+
+		$this->load->view('crud', $form);
 	}
 }
